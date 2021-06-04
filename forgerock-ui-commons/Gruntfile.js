63d2f24bf7b70ecdd7cd48b9292e592b9d2320c6
Robert Rojo <robert.rojo@orchitech.cz>
2021-06-04 17:24:50 +0200

    Change target dir of JS and CSS libs. #10
    Use Maven's target dir for JS and CSS files before copying them via Assembly plugin, instead of an arbitrary dist directory. This ensures that no old files are included in the target artifact on clean build.

--- forgerock-ui-commons/Gruntfile.js
+++ forgerock-ui-commons/Gruntfile.js
@@ -37,49 +37,49 @@ module.exports = function (grunt) {
             main: {
                 files: [
                     // JS - npm
-                    { src: "node_modules/backbone/backbone-min.js", dest: "dist/libs/backbone-1.1.2-min.js" },
-                    { src: "node_modules/backbone-relational/backbone-relational.js", dest: "dist/libs/backbone-relational-0.9.0-min.js" }, // Not actually minified
-                    { src: "node_modules/backbone.paginator/lib/backbone.paginator.min.js", dest: "dist/libs/backbone.paginator-2.0.2-min.js" },
-                    { src: "node_modules/backgrid/lib/backgrid.min.js", dest: "dist/libs/backgrid-0.3.5-min.js" },
-                    { src: "node_modules/backgrid-filter/backgrid-filter.min.js", dest: "dist/libs/backgrid-filter-0.3.7-min.js" },
-                    { src: "node_modules/backgrid-paginator/backgrid-paginator.min.js", dest: "dist/libs/backgrid-paginator-0.3.5-min.js" },
-                    { src: "node_modules/backgrid-select-all/backgrid-select-all.min.js", dest: "dist/libs/backgrid-select-all-0.3.5-min.js" },
-                    { src: "node_modules/dragula/dist/dragula.min.js", dest: "dist/libs/dragula-3.6.7-min.js" },
-                    { src: "node_modules/i18next/lib/dep/i18next.min.js", dest: "dist/libs/i18next-1.7.3-min.js" },
-                    { src: "node_modules/jquery/dist/jquery.min.js", dest: "dist/libs/jquery-2.1.1-min.js" },
-                    { src: "node_modules/moment/min/moment.min.js", dest: "dist/libs/moment-2.8.1-min.js" },
-                    { src: "node_modules/requirejs/require.js", dest: "dist/libs/requirejs-2.1.14-min.js" }, // Not actually minified
-                    { src: "node_modules/spin.js/spin.js", dest: "dist/libs/spin-2.0.1-min.js" }, // Not actually minified
-                    { src: "node_modules/xdate/src/xdate.js", dest: "dist/libs/xdate-0.8-min.js" }, // Not actually minified
-                    { src: "node_modules/react/dist/react.min.js", dest: "dist/libs/react-15.2.1-min.js" },
-                    { src: "node_modules/react-dom/dist/react-dom.min.js", dest: "dist/libs/react-dom-15.2.1-min.js" },
-                    { src: "node_modules/handlebars/dist/handlebars.js", dest: "dist/libs/handlebars-4.0.5.js" }, // Not mified but could be
+                    { src: "node_modules/backbone/backbone-min.js", dest: "target/frontend-libs/libs/backbone-1.1.2-min.js" },
+                    { src: "node_modules/backbone-relational/backbone-relational.js", dest: "target/frontend-libs/libs/backbone-relational-0.9.0-min.js" }, // Not actually minified
+                    { src: "node_modules/backbone.paginator/lib/backbone.paginator.min.js", dest: "target/frontend-libs/libs/backbone.paginator-2.0.2-min.js" },
+                    { src: "node_modules/backgrid/lib/backgrid.min.js", dest: "target/frontend-libs/libs/backgrid-0.3.5-min.js" },
+                    { src: "node_modules/backgrid-filter/backgrid-filter.min.js", dest: "target/frontend-libs/libs/backgrid-filter-0.3.7-min.js" },
+                    { src: "node_modules/backgrid-paginator/backgrid-paginator.min.js", dest: "target/frontend-libs/libs/backgrid-paginator-0.3.5-min.js" },
+                    { src: "node_modules/backgrid-select-all/backgrid-select-all.min.js", dest: "target/frontend-libs/libs/backgrid-select-all-0.3.5-min.js" },
+                    { src: "node_modules/dragula/dist/dragula.min.js", dest: "target/frontend-libs/libs/dragula-3.6.7-min.js" },
+                    { src: "node_modules/i18next/lib/dep/i18next.min.js", dest: "target/frontend-libs/libs/i18next-1.7.3-min.js" },
+                    { src: "node_modules/jquery/dist/jquery.min.js", dest: "target/frontend-libs/libs/jquery-2.1.1-min.js" },
+                    { src: "node_modules/moment/min/moment.min.js", dest: "target/frontend-libs/libs/moment-2.8.1-min.js" },
+                    { src: "node_modules/requirejs/require.js", dest: "target/frontend-libs/libs/requirejs-2.1.14-min.js" }, // Not actually minified
+                    { src: "node_modules/spin.js/spin.js", dest: "target/frontend-libs/libs/spin-2.0.1-min.js" }, // Not actually minified
+                    { src: "node_modules/xdate/src/xdate.js", dest: "target/frontend-libs/libs/xdate-0.8-min.js" }, // Not actually minified
+                    { src: "node_modules/react/dist/react.min.js", dest: "target/frontend-libs/libs/react-15.2.1-min.js" },
+                    { src: "node_modules/react-dom/dist/react-dom.min.js", dest: "target/frontend-libs/libs/react-dom-15.2.1-min.js" },
+                    { src: "node_modules/handlebars/dist/handlebars.js", dest: "target/frontend-libs/libs/handlebars-4.0.5.js" }, // Not mified but could be
 
                     // JS - custom
-                    { src: "libs/js/bootstrap-3.3.5-custom.js", dest: "dist/libs/bootstrap-3.3.5-custom.js" },
-                    { src: "libs/js/bootstrap-dialog-1.34.4-min.js", dest: "dist/libs/bootstrap-dialog-1.34.4-min.js" },
-                    { src: "libs/js/form2js-2.0-769718a.js", dest: "dist/libs/form2js-2.0-769718a.js" },
-                    { src: "libs/js/jquery.ba-dotimeout-1.0-min.js", dest: "dist/libs/jquery.ba-dotimeout-1.0-min.js" },
-                    { src: "libs/js/jquery.placeholder-2.0.8.js", dest: "dist/libs/jquery.placeholder-2.0.8.js" },
-                    { src: "libs/js/js2form-2.0-769718a.js", dest: "dist/libs/js2form-2.0-769718a.js" },
-                    { src: "libs/js/lodash-3.10.1-min.js", dest: "dist/libs/lodash-3.10.1-min.js" },
-                    { src: "libs/js/selectize-0.12.1-min.js", dest: "dist/libs/selectize-0.12.1-min.js" },
+                    { src: "libs/js/bootstrap-3.3.5-custom.js", dest: "target/frontend-libs/libs/bootstrap-3.3.5-custom.js" },
+                    { src: "libs/js/bootstrap-dialog-1.34.4-min.js", dest: "target/frontend-libs/libs/bootstrap-dialog-1.34.4-min.js" },
+                    { src: "libs/js/form2js-2.0-769718a.js", dest: "target/frontend-libs/libs/form2js-2.0-769718a.js" },
+                    { src: "libs/js/jquery.ba-dotimeout-1.0-min.js", dest: "target/frontend-libs/libs/jquery.ba-dotimeout-1.0-min.js" },
+                    { src: "libs/js/jquery.placeholder-2.0.8.js", dest: "target/frontend-libs/libs/jquery.placeholder-2.0.8.js" },
+                    { src: "libs/js/js2form-2.0-769718a.js", dest: "target/frontend-libs/libs/js2form-2.0-769718a.js" },
+                    { src: "libs/js/lodash-3.10.1-min.js", dest: "target/frontend-libs/libs/lodash-3.10.1-min.js" },
+                    { src: "libs/js/selectize-0.12.1-min.js", dest: "target/frontend-libs/libs/selectize-0.12.1-min.js" },
 
                     // CSS - npm
-                    { src: "node_modules/backgrid/lib/backgrid.min.css", dest: "dist/css/backgrid-0.3.5-min.css" },
-                    { src: "node_modules/backgrid-filter/backgrid-filter.css", dest: "dist/css/backgrid-filter-0.3.7-min.css" },
-                    { src: "node_modules/backgrid-paginator/backgrid-paginator.min.css", dest: "dist/css/backgrid-paginator-0.3.5-min.css" },
-                    { src: "node_modules/titatoggle/dist/titatoggle-dist-min.css", dest: "dist/css/titatoggle-1.2.6-min.css" }, // Actually 1.2.15
+                    { src: "node_modules/backgrid/lib/backgrid.min.css", dest: "target/frontend-libs/css/backgrid-0.3.5-min.css" },
+                    { src: "node_modules/backgrid-filter/backgrid-filter.css", dest: "target/frontend-libs/css/backgrid-filter-0.3.7-min.css" },
+                    { src: "node_modules/backgrid-paginator/backgrid-paginator.min.css", dest: "target/frontend-libs/css/backgrid-paginator-0.3.5-min.css" },
+                    { src: "node_modules/titatoggle/dist/titatoggle-dist-min.css", dest: "target/frontend-libs/css/titatoggle-1.2.6-min.css" }, // Actually 1.2.15
 
                     // CSS - custom
-                    { src: "libs/css/bootstrap-3.3.5-custom.css", dest: "dist/css/bootstrap-3.3.5-custom.css" },
-                    { src: "libs/css/bootstrap-dialog-1.34.4-min.css", dest: "dist/css/bootstrap-dialog-1.34.4-min.css" },
-                    { src: "libs/css/selectize-0.12.1-bootstrap3.css", dest: "dist/css/selectize-0.12.1-bootstrap3.css" },
+                    { src: "libs/css/bootstrap-3.3.5-custom.css", dest: "target/frontend-libs/css/bootstrap-3.3.5-custom.css" },
+                    { src: "libs/css/bootstrap-dialog-1.34.4-min.css", dest: "target/frontend-libs/css/bootstrap-dialog-1.34.4-min.css" },
+                    { src: "libs/css/selectize-0.12.1-bootstrap3.css", dest: "target/frontend-libs/css/selectize-0.12.1-bootstrap3.css" },
 
                     // Fontawesome
-                    { src: "node_modules/font-awesome/css/font-awesome.min.css", dest: "dist/css/fontawesome/css/font-awesome.min.css" },
-                    { src: "node_modules/font-awesome/less/variables.less", dest: "dist/css/fontawesome/less/variables.less" },
-                    { expand: true, flatten: true, src: "node_modules/font-awesome/fonts/*", dest: "dist/css/fontawesome/fonts/" },
+                    { src: "node_modules/font-awesome/css/font-awesome.min.css", dest: "target/frontend-libs/css/fontawesome/css/font-awesome.min.css" },
+                    { src: "node_modules/font-awesome/less/variables.less", dest: "target/frontend-libs/css/fontawesome/less/variables.less" },
+                    { expand: true, flatten: true, src: "node_modules/font-awesome/fonts/*", dest: "target/frontend-libs/css/fontawesome/fonts/" },
                 ],
             },
         },
